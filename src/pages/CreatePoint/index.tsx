24edaabfa029a29b73d429ec2841f465082e948c
Fabio Castro <fabio23.castro@gmail.com>
2021-12-07 23:18:59 -0300

    inserindo filtro por Estado

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import './styles.css';
 import logo from '../../assets/logo.svg';
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import api from '../../services/api';
 import axios from "axios";
+import {LeafletMouseEvent} from "leaflet";
 
 interface Item {
     id: number;
@@ -17,9 +18,18 @@ interface IBGEUF {
     sigla: string;
 }
 
+interface IBGECity {
+    nome: string
+}
+
 const CreatePoint = () => {
     const [items, setItems] = useState<Item[]>([]);
     const [ufs, setUF] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+
+    const [selectedUf, setSelectedUf] = useState('0');
+    const [SelectedCity, setSelectedCity] = useState('0');
+
 
 
     useEffect(() => {
@@ -35,6 +45,34 @@ const CreatePoint = () => {
             setUF(ufInitial);
         })
     }, [])
+
+    useEffect(() => {
+        if (selectedUf === '0') {
+            return;
+        }
+
+        axios.get<IBGECity[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`).then(response => {
+            const cityName = response.data.map(city => city.nome);
+
+            setCities(cityName);
+        })
+    }, [selectedUf])
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = (event.target.value);
+        setSelectedUf(uf);
+    }
+
+    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+        const city = (event.target.value);
+        setSelectedCity(city);
+    }
+
+    function handleMapClick(event: LeafletMouseEvent) {
+        console.log(event.latlng)
+    };
+    
+    
 
     return (
         <div id="page-create-point">
@@ -93,59 +131,67 @@ const CreatePoint = () => {
                     </legend>
 
 
-                    <MapContainer center={[-19.928084, -44.010364]} zoom={15} scrollWheelZoom={false}>
+                    <MapContainer center={[-19.928084, -44.010364]} 
+                    zoom={15} 
+                    onclick={handleMapClick}>
                         <TileLayer
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
                         <Marker position={[-19.928084, -44.010364]}>
-                            <Popup>
-                                A pretty CSS3 popup. <br /> Easily customizable.
-                            </Popup>
                         </Marker>
                     </MapContainer>
 
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select name="uf"
+                                id="uf"
+                                value={selectedUf}
+                                onChange={handleSelectUf}>
                                 <option value="0">Selecione uma UF</option>
-                                {ufs.map(uf =>(
+                                {ufs.map(uf => (
                                     < option key={uf} value={uf}>{uf}</option>
-                            ))}
-                        </select>
+                                ))}
+                            </select>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city"
+                                value={SelectedCity}
+                                onChange={handleSelectCity}
+                                id="city">
+                                <option value="0">Selecione uma Cidade</option>
+                                {cities.map(city => (
+                                    < option key={city} value={city}>{city}</option>
+                                ))}
+                            </select>
+                        </div>
                     </div>
+                </fieldset>
 
-                    <div className="field">
-                        <label htmlFor="city">Cidade</label>
-                        <select name="city" id="city">
-                            <option value="0">Selecione uma Cidade</option>
-                        </select>
-                    </div>
-                </div>
-            </fieldset>
+                <fieldset>
+                    <legend>
+                        <h2>Itens de coleta</h2>
+                        <span>Selecione um ou mais itens abaixo</span>
+                    </legend>
+                    <ul className="items-grid">
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
 
-            <fieldset>
-                <legend>
-                    <h2>Itens de coleta</h2>
-                    <span>Selecione um ou mais itens abaixo</span>
-                </legend>
-                <ul className="items-grid">
-                    {items.map(item => (
-                        <li key={item.id}>
-                            <img src={item.image_url} alt={item.title} />
-                            <span>{item.title}</span>
-                        </li>
-                    ))}
+                    </ul>
+                </fieldset>
 
-                </ul>
-            </fieldset>
+                <button type="submit">
+                    Cadastrar ponto de coleta
+                </button>
 
-            <button type="submit">
-                Cadastrar ponto de coleta
-            </button>
-
-        </form>
+            </form>
         </div >
     )
 };
